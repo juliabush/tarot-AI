7668a5d7872b4adc38925360ce17d0d2dd0b4995
juliabush <j.elizabethbush@gmail.com>
2025-03-21 14:43:10 +0100

    getting the suer input as a prompt for chat

--- main.js
+++ main.js
@@ -5,17 +5,21 @@ import OpenAI from "openai";
 const client = new OpenAI({
     apiKey: process.env.API_KEY
 });
+const userSubmitButton = document.getElementById("user-submit-button");
+const userInput = document.getElementById("user-input");
+const dialogPopUp = document.getElementById("dialog-popup");
 
-async function getChatResponse() {
+userSubmitButton.addEventListener("click", async (event) => {
+    event.preventDefault();
     try {
         const completion = await client.chat.completions.create ({
             model: "gpt-4o",
-            messages: [{ role:"user", content:"Hello ChatGPT"}]
+            messages: [{ role:"user", content: userInput.value}]
         });
-        console.log("ChatGPT:", completion.choices[0].message.content);
+        const aiResponse = completion.choices[0].message.content;
+        dialogPopUp.innerHTML = `<p>${aiResponse}</p>`;
+        dialogPopUp.showModal();
     } catch (error) {
         console.error("Error:", error);
     }
-}
-
-getChatResponse();
+});
